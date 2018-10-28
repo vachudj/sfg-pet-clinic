59b124dabc35e1396359113f95e73d53c8cba459
Jan Vachuda <vachudj@gmail.com>
2018-10-28 15:37:50 +0100

    Adding POJOs. Closes #2.

---
+++ src/main/java/com/springtraining/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.springtraining.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/springtraining/sfgpetclinic/model/Vat.java
@@ -0,0 +1,4 @@
+package com.springtraining.sfgpetclinic.model;
+
+public class Vat extends Person {
+}
