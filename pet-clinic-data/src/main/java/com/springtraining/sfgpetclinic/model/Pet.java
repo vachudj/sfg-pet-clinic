4f7cb1cce812f0a0bd5b7aa56ec5825bae07bbde
Jan Vachuda <vachudj@gmail.com>
2018-10-29 18:55:35 +0100

    Adding BaseEntity. Closes #10.

--- pet-clinic-data/src/main/java/com/springtraining/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/springtraining/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.springtraining.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
