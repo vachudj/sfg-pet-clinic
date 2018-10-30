eaeab5a6a87f3bd5892a24104d5d3479de38b960
Jan Vachuda <vachudj@gmail.com>
2018-10-30 19:38:46 +0100

    Imp Map Based Services. Closes #6.

---
+++ pet-clinic-data/src/main/java/com/springtraining/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package com.springtraining.sfgpetclinic.services.map;
+
+import com.springtraining.sfgpetclinic.model.Vet;
+import com.springtraining.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
