ac7b0a8c760e88fab6c2f7c4b9833ba7e6be2c61
Jan Vachuda <vachudj@gmail.com>
2018-11-06 18:57:34 +0100

    Added PetType to bootstrap. Closes #32.

--- pet-clinic-web/src/main/java/com/springtraining/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springtraining/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,15 +1,16 @@
 package com.springtraining.sfgpetclinic.bootstrap;
 
-import static org.slf4j.LoggerFactory.*;
-
+import com.springtraining.sfgpetclinic.model.Owner;
+import com.springtraining.sfgpetclinic.model.PetType;
+import com.springtraining.sfgpetclinic.model.Vet;
+import com.springtraining.sfgpetclinic.services.OwnerService;
+import com.springtraining.sfgpetclinic.services.PetTypeService;
+import com.springtraining.sfgpetclinic.services.VetService;
 import org.slf4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import com.springtraining.sfgpetclinic.model.Owner;
-import com.springtraining.sfgpetclinic.model.Vet;
-import com.springtraining.sfgpetclinic.services.OwnerService;
-import com.springtraining.sfgpetclinic.services.VetService;
+import static org.slf4j.LoggerFactory.getLogger;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -20,13 +21,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final VetService vetService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    private final PetTypeService petTypeService;
+
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Glemanne");
