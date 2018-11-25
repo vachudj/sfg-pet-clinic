b68db31b2127e963fae3d34c5422b326cb5f951f
Jan Vachuda <vachudj@gmail.com>
2018-11-25 07:15:53 +0100

    Added spring data jpa service for speciality. Closes #44.

---
+++ pet-clinic-data/src/main/java/com/springtraining/sfgpetclinic/services/springdatajpa/SpecialitySDJpaService.java
@@ -0,0 +1,48 @@
+package com.springtraining.sfgpetclinic.services.springdatajpa;
+
+import com.springtraining.sfgpetclinic.model.Speciality;
+import com.springtraining.sfgpetclinic.repositories.SpecialityRepository;
+import com.springtraining.sfgpetclinic.services.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialitySDJpaService implements SpecialityService {
+
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialitySDJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialitys = new HashSet<>();
+        specialityRepository.findAll().forEach(specialitys::add);
+        return specialitys;
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return specialityRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialityRepository.deleteById(id);
+    }
+}
